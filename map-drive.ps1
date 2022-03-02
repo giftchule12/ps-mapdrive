9714fed142769d3ce8f093592442e5a65ca51696
giftchule12 <giftchule@gmail.com>
2022-03-02 11:27:45 +0200

    mitigate powershell 3 and below bug

--- map-drive.ps1
+++ map-drive.ps1
@@ -1,4 +1,5 @@
-﻿Write-Output "
+﻿Write-Output "Property of "
+Write-Output "
 ███╗░░░███╗██╗████████╗
 ████╗░████║██║╚══██╔══╝
 ██╔████╔██║██║░░░██║░░░
@@ -6,46 +7,42 @@
 ██║░╚═╝░██║██║░░░██║░░░
 ╚═╝░░░░░╚═╝╚═╝░░░╚═╝░░░"
 
-# $Credentials = Get-Credential -Credential Computer
-# $Credentials.Password | ConvertFrom-SecureString | Set-Content password.txt
-# $Username = Get-Credential -Credential admin
-
-# $Password = Get-Content "password.txt" | ConvertTo-SecureString
-
 Write-Output "This program will automatically create a network drive"
 
 Write-Output "If you agree with the terms of the program click press Enter"
 
 Read-Host -prompt "Press Enter to Continue"
 
-function deleteDrive {
-    Write-Output "4"
-
-    Write-Output "Drive is in use"
-
-    Get-PSDrive Z | Remove-PSDRive
-
-    
-    net stop workstation /y
-
-    net start workstation
-}
-function createDrive{
-    Write-Output "2"
-
-    Write-Output "Lets's Create a New One!!!"
-
-    New-PSDrive -Name "Z" -PSProvider "FileSystem" -Root "\\MITSERVER\Pastel14" -Persist -Credential $Cred
-}
-
-
 $Username = "Computer"
+$domain =  "\\MITSERVER\Pastel14"
 $Password = ConvertTo-SecureString "22382238" -AsPlainText -Force
 $Cred = New-Object System.Management.Automation.PSCredential ($Username, $Password)
 
-Get-PSDrive Z
+$version = $PSVersionTable.PSVersion.Major
 
-if (Get-PSDrive Z -ErrorAction SilentlyContinue){
+if ($version -le 3){
+    Write-Output "Lets's Create a New One!!!"
+    
+    $net = New-Object -ComObject WScript.Network
+    $net.MapNetworkDrive("Z:", $domain, $false, $Username, "22382238")
+
+    Get-PSDrive Z -PSProvider "Filesystem"
+
+    Write-Output  "Drive Succesfully Mapped on $domain" 
+}
+else{
+    Write-Output "Lets's Create a New One!!!"
+    # Remove-PSDRive Z
+
+    New-PSDrive -Name "Z" -PSProvider "FileSystem" -Root $domain -Persist -Credential $Cred
+
+    Get-PSDrive Z -PSProvider "Filesystem"
+
+    Write-Output  "Drive Succesfully Mapped on $domain"
+}
+
+
+<# if (Get-PSDrive Z -ErrorAction SilentlyContinue){
     Write-Output "Drive is in use"
 
     Get-PSDrive Z | Remove-PSDRive
@@ -60,7 +57,7 @@ else{
 
     New-PSDrive -Name "Z" -PSProvider "FileSystem" -Root "\\MITSERVER\Pastel14" -Persist -Credential $Cred
 }
-
+ #>
 Read-Host -Prompt "Press Enter to Exit"
 
 
